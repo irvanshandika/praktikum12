bbd0b4aafd5d6eea48b750968c92781d3775fd8d
irvanshandika <shandikamuhammadirvan@gmail>
2023-07-08 13:24:06 +0700

    Fix request

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -28,6 +28,7 @@ const showingNavigationDropdown = ref(false);
               <!-- Navigation Links -->
               <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                 <NavLink :href="route('dashboard')" :active="route().current('dashboard')"> Dashboard </NavLink>
+                <NavLink :href="route('posts.index')" :active="route().current('dashboard')"> Posts </NavLink>
               </div>
             </div>
 
